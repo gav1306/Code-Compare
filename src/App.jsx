dddc9d8fa42529e09343292c14df33a53fb69d59
Gayatri Patil <108718904+gav1306@users.noreply.github.com>
2024-08-28 16:44:59 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,7 @@ function App() {
       setHighlightedSearch(highlightedParagraph);
     } else {
       setHighlightedSearch(text.input);
+      setSelectedText("")
     }
   }, [selectedText, startCompare, text.input]);
 
